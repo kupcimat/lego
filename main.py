02acf7bb2c2452cb66fe3d8a9aae4c1e2a825708
Matej Kupciha <kupciham@hotmail.com>
2021-01-03 17:46:41 +0100

    Extract products from result json

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from dataclasses import asdict, dataclass
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import aiohttp
 
@@ -15,6 +15,41 @@ class Query:
     operationName: str
     query: str
     variables: Dict[str, Any]
+
+
+@dataclass
+class Product:
+    code: str
+    name: str
+    availability: str
+    availability_text: str
+    max_order_quantity: int
+    price: int
+    price_text: str
+    currency: str
+
+
+def extract_products(json_data: Any) -> List[Product]:
+    sections = [
+        section
+        for section in json_data["data"]["contentPage"]["sections"]
+        if section["__typename"] == "DisruptorProductSection"
+    ]
+    products = sections[0]["products"]["results"]
+
+    return [
+        Product(
+            code=product["productCode"],
+            name=product["name"],
+            availability=product["variant"]["attributes"]["availabilityStatus"],
+            availability_text=product["variant"]["attributes"]["availabilityText"],
+            max_order_quantity=product["variant"]["attributes"]["maxOrderQuantity"],
+            price=product["variant"]["price"]["centAmount"],
+            price_text=product["variant"]["price"]["formattedAmount"],
+            currency=product["variant"]["price"]["currencyCode"],
+        )
+        for product in products
+    ]
 
 
 async def main():
@@ -34,8 +69,10 @@ async def main():
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=asdict(query), headers=headers) as response:
             logging.info(f"action=get-data status={response.status}")
-            data = await response.text()
-            print(data)
+            data = await response.json()
+            products = extract_products(data)
+            for product in products:
+                print(product)
 
 
 asyncio.run(main())
