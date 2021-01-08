0a524d73a4f8f472d389e0b6ea7d8e597d3ac30b
Matej Kupciha <kupciham@hotmail.com>
2021-01-08 13:09:31 +0100

    Use itertools to flatten lists

--- kupcimat/utils.py
+++ kupcimat/utils.py
@@ -1,10 +1,13 @@
 from dataclasses import fields
-from typing import Any, List
+from itertools import chain
+from typing import Any, List, TypeVar
+
+T = TypeVar("T")
 
 
 def field_names(class_or_instance: Any) -> List[str]:
     return [field.name for field in fields(class_or_instance)]
 
 
-def flatten(list_of_lists: List[Any]):
-    return [item for sublist in list_of_lists for item in sublist]
+def flatten(list_of_lists: List[List[T]]) -> List[T]:
+    return list(chain.from_iterable(list_of_lists))
